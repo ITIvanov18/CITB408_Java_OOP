41055edac33b5aa8aa5b68ef91876578e0f2b618
ITIvanov18 <ITIvanov18@codingburgas.bg>
2025-03-08 14:38:15 +0200

    Commit class Engine from Topic 2. OOP. Inheritance and polymorphism

---
+++ Topic 2. OOP. Inheritance and polymorphism/src/Vehicles/Engine.java
@@ -0,0 +1,14 @@
+package Vehicles;
+
+public class Engine {
+
+    private double engineVolume;
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+}
