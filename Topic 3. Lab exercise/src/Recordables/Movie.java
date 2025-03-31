18c9cb70b46daf11679ffc8eef2e43ab3e992525
ITIvanov18 <ITIvanov18@codingburgas.bg>
2025-03-31 23:54:00 +0300

    Commit class Movie from Topic 3. Lab exercise

---
+++ Topic 3. Lab exercise/src/Recordables/Movie.java
@@ -0,0 +1,38 @@
+package Recordables;
+
+public class Movie extends ObjectWithDuration {
+    private String title;
+    private double duration;
+    private String genre;
+
+    public Movie(String title, double duration, String genre) {
+        this.title = title;
+        this.duration = duration;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
+
